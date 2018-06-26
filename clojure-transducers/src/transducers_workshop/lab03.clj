80e351b82e5652bfa81a00618535e479a02872c5
reborg <reborg@reborg.net>
2018-06-26 17:35:12 +0100

    unit 3 core.async

--- clojure-transducers/src/transducers_workshop/lab03.clj
+++ clojure-transducers/src/transducers_workshop/lab03.clj
@@ -5,13 +5,15 @@
     [clojure.core.async :refer [go go-loop chan >! <! <!! close!]]))
 
 ; Welcome to lab3: attaching a transducer chain to a core.async process.
+
 ; In this lab we simulate a stream of incoming financial products.
 ; The products are nested maps of values, lists or other data.
 ; In lab 1 we developed a transducer to prepare this data for searching.
 ; The preparation step can be attached directly to the stream of
-; incoming (raw) products so when we need to search for them they are ready.
-; We are going to save already prepared products in a local cache in memory.
-; The follwing are functions and variables helpers to use in the next steps:
+; incoming (raw) products so we can search them directly.
+; We are going to save already prepared products in a local cache.
+
+; The following are functions helpers to be used in the tasks:
 
 (defn load-data [] (edn/read-string (slurp "feed.edn")))
 (def cache (atom []))
@@ -41,8 +43,7 @@
       (swap! cache conj item)
       (recur))))
 
-; Step1: create an input and output channel with buffer size 1.
-; See the labs slides for the correct syntax.
+; ############ Task 1: create an input and output channel with buffer size 1.
 
 (def in
   ;... complete here
@@ -56,25 +57,24 @@
 ; thread putting it in a separate thread with "future".
 (future (consume out))
 
-; STEP 2: stream some products into the input channel.
+; ######### Task 2: stream some products into the input channel.
 ; The consumer will start consuming from the "future" thread.
 
 (to-stream
   ; complete here
   )
 
-; STEP 3: check that something ended up in the cache.
-; How does it look like? Is that ready for filtering or it requires "preparation"?
-; Grab the first product and print it on screen. To access an atom, you will need
-; to defer it with "@" before. To stop the channels use (stop!) on the channel.
-; Reset the cache so you can see new results later. To do this, you can just redefine the var.
+; ########## Task 3: check that something ended up in the cache.
+
+; Are the product in the cache ready for searching or they require preparation?
+; Grab the first product and print it on screen. To stop the channels use
+; (stop!) on the channel. Reset the cache so you can see new results later.
+; To do this, you can just redefine the var.
 
 (println (first "Check the cache here"))
 
-; STEP 4: repeat the experience by attaching the "prepare-data"
-; transducer chain to the input or output channel.
-; Create the input and output channel. This time be sure to attach the transducer chain in one of them.
-; What is the difference?
+; ############ Task 4: repeat the experience by attaching an xform
+; Create the input and output channel. This time be sure to attach the transducer chain in one of them. How does it change if you put the xform in one channel or the other? What it there are many consumers?
 
 (def in
   ;... complete here
